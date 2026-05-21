bfdf07075089dfb62999efbe63cf6bc83f2928ef
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-21 18:36:20 -0300

    feat: configura urls de autenticacao

--- hotelapp/settings.py
+++ hotelapp/settings.py
@@ -132,5 +132,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'), 
 ]
 
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
