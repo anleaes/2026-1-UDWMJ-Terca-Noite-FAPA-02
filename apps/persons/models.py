b99e1e6c18294a435200f7989e33fb4e962036f8
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-21 18:52:01 -0300

    feat: define Person como classe abstrata

--- apps/persons/models.py
+++ apps/persons/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField('Nome', max_length=50)
+    last_name = models.CharField('Sobrenome', max_length=100)
+    address = models.CharField('Endereço', max_length=200)
+    phone = models.CharField('Telefone', max_length=20)
+    email = models.EmailField('E-mail')
+
+    class Meta:
+        abstract = True
+        ordering = ['first_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
