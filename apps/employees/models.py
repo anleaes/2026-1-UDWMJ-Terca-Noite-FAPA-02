a3299a02752d1dad70647b2223b60c05b4283413
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-21 19:08:07 -0300

    feat: define modelo Employee e registra no admin

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from persons.models import Person
 
-# Create your models here.
+
+class Employee(Person):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='employee_profile', null=True, blank=True, verbose_name='Usuário')
+    salary = models.FloatField('Salário')
+    position = models.CharField('Cargo', max_length=50)
+    hired_date = models.DateField('Data de contratação')
+    is_active = models.BooleanField('Ativo', default=True)
+    contract = models.FileField('Contrato', upload_to='contracts/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+        ordering = ['first_name']
