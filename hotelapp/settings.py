84860e787d25008ead3854ceb603c62773accd26
Jcolares34 <jcolares34@gmail.com>
2026-05-21 16:02:55 -0300

    feat: cria app services e registra em INSTALLED_APPS

--- hotelapp/settings.py
+++ hotelapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'service_categories.apps.ServiceCategoriesConfig',
+    'services.apps.ServicesConfig',
 ]
 
 MIDDLEWARE = [
