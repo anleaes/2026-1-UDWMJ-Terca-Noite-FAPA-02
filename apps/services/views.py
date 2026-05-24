c18b5faf520c0e01c70e53006548ac10b1fb09c4
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-24 17:01:47 -0300

    refactor: reescreve views de Service usando FBV com login_required

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,53 +1,61 @@
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import ServiceForm
 from .models import Service
 
 
-class ServiceListView(ListView):
-	model = Service
-	template_name = 'services/list_services.html'
-	context_object_name = 'services'
-
-	def get_queryset(self):
-		queryset = super().get_queryset()
-		query = self.request.GET.get('q')
-
-		if query:
-			queryset = queryset.filter(
-				Q(name__icontains=query)
-				| Q(description__icontains=query)
-				| Q(category__name__icontains=query)
-			)
-
-		active = self.request.GET.get('active')
-		if active == '1':
-			queryset = queryset.filter(is_active=True)
-		elif active == '0':
-			queryset = queryset.filter(is_active=False)
-
-		return queryset
+def list_services(request):
+    template_name = 'services/list_services.html'
+    services = Service.objects.select_related('category').filter(is_active=True)
+    context = {'services': services}
+    return render(request, template_name, context)
 
 
-class ServiceCreateView(CreateView):
-	model = Service
-	form_class = ServiceForm
-	template_name = 'services/add_service.html'
-	success_url = reverse_lazy('services:list')
+@login_required(login_url='/accounts/login/')
+def add_service(request):
+    template_name = 'services/add_service.html'
+    context = {}
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('services:list_services')
+    else:
+        form = ServiceForm()
+    context['form'] = form
+    return render(request, template_name, context)
 
 
-class ServiceUpdateView(UpdateView):
-	model = Service
-	form_class = ServiceForm
-	template_name = 'services/add_service.html'
-	success_url = reverse_lazy('services:list')
+@login_required(login_url='/accounts/login/')
+def edit_service(request, id_service):
+    template_name = 'services/add_service.html'
+    service = get_object_or_404(Service, id=id_service)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('services:list_services')
+    else:
+        form = ServiceForm(instance=service)
+    return render(request, template_name, {'form': form})
 
 
-class ServiceDeleteView(DeleteView):
-	model = Service
-	template_name = 'services/delete_service.html'
-	success_url = reverse_lazy('services:list')
+@login_required(login_url='/accounts/login/')
+def delete_service(request, id_service):
+    service = get_object_or_404(Service, id=id_service)
+    service.delete()
+    return redirect('services:list_services')
 
-# Create your views here.
+
+def search_services(request):
+    template_name = 'services/list_services.html'
+    query = request.GET.get('query', '')
+    services = Service.objects.select_related('category').filter(
+        Q(name__icontains=query)
+        | Q(description__icontains=query)
+        | Q(category__name__icontains=query)
+    )
+    context = {'services': services, 'query': query}
+    return render(request, template_name, context)
