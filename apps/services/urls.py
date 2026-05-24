ddf901df80db7160c076cb32dcb9ad18e06f4da5
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-24 17:02:12 -0300

    refactor: atualiza urls de services para o padrao do projeto

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'services'
 
 urlpatterns = [
-    path('listar/', views.ServiceListView.as_view(), name='list'),
-    path('adicionar/', views.ServiceCreateView.as_view(), name='add'),
-    path('editar/<int:pk>/', views.ServiceUpdateView.as_view(), name='edit'),
-    path('excluir/<int:pk>/', views.ServiceDeleteView.as_view(), name='delete'),
+    path('listar/', views.list_services, name='list_services'),
+    path('adicionar/', views.add_service, name='add_service'),
+    path('editar/<int:id_service>/', views.edit_service, name='edit_service'),
+    path('excluir/<int:id_service>/', views.delete_service, name='delete_service'),
+    path('buscar/', views.search_services, name='search_services'),
 ]
