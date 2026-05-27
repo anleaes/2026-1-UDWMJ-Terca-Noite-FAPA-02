3e802924a27ecc4542189aa15b306a02d4d4c0f5
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-27 19:41:48 -0300

    feat: expoe Review via REST com permissoes por action

--- apps/reviews/urls.py
+++ apps/reviews/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 app_name = 'reviews'
+
+router = routers.SimpleRouter()
+router.register('', views.ReviewViewSet, basename='api-reviews')
 
 urlpatterns = [
     path('listar/', views.list_reviews, name='list_reviews'),
@@ -10,4 +14,5 @@ urlpatterns = [
     path('editar/<int:id_review>/', views.edit_review, name='edit_review'),
     path('excluir/<int:id_review>/', views.delete_review, name='delete_review'),
     path('buscar/', views.search_reviews, name='search_reviews'),
+    path('', include(router.urls)),
 ]
