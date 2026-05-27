16bb9362209f2eb7c5eeed144f577c593613fbef
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-27 19:32:03 -0300

    feat: expoe Service via REST

--- apps/services/urls.py
+++ apps/services/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 app_name = 'services'
+
+router = routers.SimpleRouter()
+router.register('', views.ServiceViewSet, basename='api-services')
 
 urlpatterns = [
     path('listar/', views.list_services, name='list_services'),
@@ -10,4 +14,5 @@ urlpatterns = [
     path('editar/<int:id_service>/', views.edit_service, name='edit_service'),
     path('excluir/<int:id_service>/', views.delete_service, name='delete_service'),
     path('buscar/', views.search_services, name='search_services'),
+    path('', include(router.urls)),
 ]
