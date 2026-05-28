951e78f8adad68f15b50e2b7052508fbc9483b79
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-28 06:25:55 -0300

    feat: expoe Invoice via REST com get_queryset filtrado

--- apps/invoices/urls.py
+++ apps/invoices/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 app_name = 'invoices'
+
+router = routers.SimpleRouter()
+router.register('', views.InvoiceViewSet, basename='api-invoices')
 
 urlpatterns = [
     path('listar/', views.list_invoices, name='list_invoices'),
@@ -10,4 +14,5 @@ urlpatterns = [
     path('editar/<int:id_invoice>/', views.edit_invoice, name='edit_invoice'),
     path('excluir/<int:id_invoice>/', views.delete_invoice, name='delete_invoice'),
     path('buscar/', views.search_invoices, name='search_invoices'),
+    path('', include(router.urls)),
 ]
