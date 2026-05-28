219961642c8c07ae5b05d9714d60e8cd4f754d7c
Pedro Kunz <pedrokunz094@gmail.com>
2026-05-28 06:23:55 -0300

    feat: expoe ReservationItem via REST

--- apps/reservation_items/urls.py
+++ apps/reservation_items/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 app_name = 'reservation_items'
+
+router = routers.SimpleRouter()
+router.register('', views.ReservationItemViewSet, basename='api-reservation-items')
 
 urlpatterns = [
     path('listar/', views.list_reservation_items, name='list_reservation_items'),
@@ -10,4 +14,5 @@ urlpatterns = [
     path('editar/<int:id_item>/', views.edit_reservation_item, name='edit_reservation_item'),
     path('excluir/<int:id_item>/', views.delete_reservation_item, name='delete_reservation_item'),
     path('buscar/', views.search_reservation_items, name='search_reservation_items'),
+    path('', include(router.urls)),
 ]
